1d62f2c0f0355e14dca0b0c7c557d917d5543746
Gabriel <gabriellagrota23@gmail.com>
2023-10-19 14:05:07 -0300

    Feat: Estrutura do serviço de construção de CPF construída

---
+++ src/main/java/br/com/rcbr/svc/cpf/models/responses/CpfResponse.java
@@ -0,0 +1,12 @@
+package br.com.rcbr.svc.cpf.models.responses;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class CpfResponse {
+    String cpf;
+}
